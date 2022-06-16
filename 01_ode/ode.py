aa55d34888755311d29194ccf0f626eb2a1edf88
Gary Bake <garybake@gmail.com>
2022-06-16 08:46:43 +0100

    Change to 1 based arrays - sad face

--- 01_ode/ode.py
+++ 01_ode/ode.py
@@ -1,5 +1,6 @@
 """
 ODE - PAT HALL, 2/86
+Code is written to be close to original so not pythonic
 """
 import random
 
@@ -8,34 +9,30 @@ from ode_words_list import word_idx, words
 def initialise():
   r = word_idx
 
-  curr_words_idx = 0
+  curr_words_idx = 1
 
   # nouns, adjectives, verbs classified into 5 categories of rhyme
   h, w, d = 3, 5, 7
-  r_s = [[[None for x in range(d)] for x in range(w)] for y in range(h)] 
-  for i in range(1, 4):
-    for j in range(1, 6):
-      for k in range(1, r[i-1][j-1]+1):
-        r_s[i-1][j-1][k-1] = words[curr_words_idx]
+  r_s = [[[None for x in range(d+1)] for x in range(w+1)] for y in range(h+1)] 
+  for i in range(1, 3+1):
+    for j in range(1, 5+1):
+      for k in range(1, r[i][j]+1):
+        r_s[i][j][k] = words[curr_words_idx]
         curr_words_idx += 1
   
   # Non rhyming nouns
-  n_s = [None for x in range(10)]
-  for i in range(1, 11):
-    n_s[i-1] = words[curr_words_idx]
+  n_s = [None for x in range(10+1)]
+  for i in range(1, 10+1):
+    n_s[i] = words[curr_words_idx]
     curr_words_idx += 1
-
+  
   # Non rhyming adjectives
-  a_s = [None for x in range(10)]
-  for i in range(1, 11):
-    a_s[i-1] = words[curr_words_idx]
+  a_s = [None for x in range(10+1)]
+  for i in range(1, 10+1):
+    a_s[i] = words[curr_words_idx]
     curr_words_idx += 1
 
   return r_s, n_s, a_s
-
-  # print(r_s)
-  # print(n_s)
-  # print(a_s)
 
 def prompt():
   name = 'Gary'
@@ -68,38 +65,71 @@ def do_random(s):
   return random.randint(1, s)
 
 def do_couplet(r_flag, c_flag, r_s, n_s, a_s, name):
+  c_flag = 2  # TODO remove
   funcs = {
     1: couplet_1,
-    2: couplet_1,
-    3: couplet_1,
-    4: couplet_1,
-    5: couplet_1,
+    2: couplet_2,
+    3: couplet_3,
+    4: couplet_4,
+    5: couplet_5,
   }
   funcs[c_flag](r_flag, c_flag, r_s, n_s, a_s, name)
 
 def couplet_1(r_flag, c_flag, r_s, n_s, a_s, name):
-  print('Doing couplet 1')
+  # print('Doing couplet 1')
   n = do_random(10)
-  adjective = a_s[n-1]
+  adjective = a_s[n]
 
-  s = word_idx[0][r_flag-1]
+  s = word_idx[1][r_flag]
   n = do_random(s)
-  noun = r_s[0][r_flag][n-1]
+  noun = r_s[1][r_flag][n]
 
-  phrase = f'I LOVE TO WATCH YOUR {adjective} {noun}'
+  phrase = f'I LOVE TO WATCH YOUR {adjective} {noun},'
   print(phrase)
 
+  s = word_idx[3][r_flag]
+  n = do_random(s)
+  word = r_s[3][r_flag][n]
 
-def couplet_2(r_s, r_f, n_s, a_s, name):
+  phrase = f'DESPITE THE FACT YOU SEEM TO {word}.'
+  print(phrase)
+
+def couplet_2(r_flag, c_flag, r_s, n_s, a_s, name):
   print('Doing couplet 2')
 
-def couplet_3(r_s, r_f, n_s, a_s, name):
+  n = do_random(10)
+  adjective = a_s[n]
+
+  n = do_random(10)
+  noun = n_s[n]
+
+  s = word_idx[1][r_flag]
+  n = do_random(s)
+  word = r_s[1][r_flag][n]
+
+  phrase = f'THE {adjective} {noun} REMINDS ME OF YOUR {word},'
+  print(phrase)
+
+  n = do_random(5)
+  rn = n
+  s = word_idx[1][rn]
+  n = do_random(s)
+  noun = r_s[1][rn][n]
+
+  s = word_idx[2][r_flag]
+  n = do_random(s)
+  adjective = r_s[2][r_flag][n]
+
+  phrase = f'EVEN THOUGH YOUR {noun} IS RATHER {adjective}.'
+  print(phrase)
+
+def couplet_3(r_flag, c_flag, r_s, n_s, a_s, name):
   print('Doing couplet 3')
 
-def couplet_4(r_s, r_f, n_s, a_s, name):
+def couplet_4(r_flag, c_flag, r_s, n_s, a_s, name):
   print('Doing couplet 4')
 
-def couplet_5(r_s, r_f, n_s, a_s, name):
+def couplet_5(r_flag, c_flag, r_s, n_s, a_s, name):
   print('Doing couplet 5')
 
 def control():
